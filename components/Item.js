a3fd5c74160f0e958e0ada37165968daaeac0586
laniywh <laniywh@gmail.com>
2019-10-10 22:26:48 -0700

    Fix typo

--- components/Item.js
+++ components/Item.js
@@ -7,9 +7,10 @@ import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 import formatMoney from '../lib/formatMoney';
 import DeleteItem from './DeleteItem';
+import AddToCart from './AddToCart';
 
 export default class Item extends Component {
-  static PropTypes = {
+  static propTypes = {
     item: PropTypes.object.isRequired,
   };
 
@@ -41,7 +42,7 @@ export default class Item extends Component {
           >
             <a>Edit</a>
           </Link>
-          <button>Add to cart</button>
+          <AddToCart id={item.id} />
           <DeleteItem id={item.id}>Delete This Item</DeleteItem>
         </div>
       </ItemStyles>
