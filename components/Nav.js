02ca3ea54bbddd55bf2ceda62f8f15de14a21b5a
laniywh <laniywh@gmail.com>
2019-10-14 14:32:15 -0700

    Add cart count in nav

--- components/Nav.js
+++ components/Nav.js
@@ -4,6 +4,7 @@ import { TOGGLE_CART_MUTATION } from './Cart';
 import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
+import CartCount from './CartCount';
 
 const Nav = () => (
   <User>
@@ -28,6 +29,9 @@ const Nav = () => (
               {toggleCart => (
                 <button type="button" onClick={toggleCart}>
                   My Cart
+                  <CartCount
+                    count={me.cart.reduce((count, cartItem) => count + cartItem.quantity, 0)}
+                  />
                 </button>
               )}
             </Mutation>
