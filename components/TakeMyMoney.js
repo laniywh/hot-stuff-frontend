b7249e4d3570ca90e678b65f8db23bfff852e53b
laniywh <laniywh@gmail.com>
2019-10-14 18:49:52 -0700

    Use stripe to create order

---
+++ components/TakeMyMoney.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import { Mutation } from 'react-apollo';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import PropTypes from 'prop-types';
+import gql from 'graphql-tag';
+import calcTotalPrice from '../lib/calcTotalPrice';
+import Error from './ErrorMessage';
+import User, { CURRENT_USER_QUERY } from './User';
+
+const CREATE_ORDER_MUTATION = gql`
+  mutation CREATE_ORDER_MUTATION($token: String!) {
+    createOrder(token: $token) {
+      id
+      charge
+      total
+      items {
+        id
+        title
+      }
+    }
+  }
+`;
+
+function totalItems(cart) {
+  return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
+}
+
+class TakeMyMoney extends React.Component {
+  onToken = async (res, createOrder) => {
+    const order = await createOrder({
+      variables: {
+        token: res.id,
+      },
+    }).catch(err => {
+      alert(err.message);
+    });
+    console.log(order);
+  };
+
+  render() {
+    return (
+      <User>
+        {({ data: { me } }) => (
+          <Mutation
+            mutation={CREATE_ORDER_MUTATION}
+            refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+          >
+            {createOrder => (
+              <StripeCheckout
+                amount={calcTotalPrice(me.cart)}
+                name="Hot Stuff"
+                description={`Order of ${totalItems(me.cart)} items`}
+                image={me.cart.length && me.cart[0].item && me.cart[0].item.image}
+                stripeKey="pk_test_zuFjJLb2wK6VC7Yb11zbOSva00o5Ek3cdG"
+                currency="USD"
+                email={me.email}
+                token={res => this.onToken(res, createOrder)}
+              >
+                {this.props.children}
+              </StripeCheckout>
+            )}
+          </Mutation>
+        )}
+      </User>
+    );
+  }
+}
+
+export default TakeMyMoney;
